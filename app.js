b55af5ceb5139d98c7d691633aa1be855a897eb0
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-06-07 23:05:38 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require('@google-cloud/debug-agent').start({ allowExpressions: true })
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
+
+app.get('/hello', (req, res) => {
+    const name = req.query.name
+
+    res.send(`<h1>Hello ${name}</h1>`)
+})
+
+app.get('/error', (req, res) => {
+    // const name = "Dicoding"
+    res.send(`<h1>Hello ${name}</h1>`)
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log(`Server is up and listening at port: ${PORT}`)
+})
+
+module.exports = app
---
+++ test.js
@@ -0,0 +1,45 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('./app')
+
+chai.use(chaiHttp)
+chai.should()
+
+describe('Simple test', () => {
+    it('GET / should return 200', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200)
+                done()
+            })
+    })
+
+    it('GET /hello should return 200', (done) => {
+        chai.request(app)
+            .get('/hello')
+            .query({name: 'Dicoding'})
+            .end((err, res) => {
+                res.should.have.status(200)
+                done()
+            })
+    })
+
+    it('GET /notfound should return 404', (done) => {
+        chai.request(app)
+            .get('/notfound')
+            .end((err, res) => {
+                res.should.have.status(404)
+                done()
+            })
+    })
+
+    it('GET /error should return 500', (done) => {
+        chai.request(app)
+            .get('/error')
+            .end((err, res) => {
+                res.should.have.status(500)
+                done()
+            })
+    })
+})
